60b693bb85977638811414fab372b10f350f45b5
krishna teja <92019396+GV-KrishnaTeja@users.noreply.github.com>
2021-10-08 09:32:17 +0530

    initial commit

---
+++ floatType.js
@@ -0,0 +1,41 @@
+const xyz = parseInt(9.1236);
+//console.log(xyz);
+
+
+
+const x = parseInt("hello.kki");
+console.log(x);
+
+const num1 = "1234.24";
+console.log(parseFloat(num1));
+
+const kit =(13.34-2.11);
+console.log(parseInt(kit));                      //float will  not give roundfig 
+                                                  //iint will  give round fig
+
+
+const hyd =('hai' );
+console.log(parseFloat(hyd))      
+
+
+const ban=('34, 24 ,66')
+console.log(parseFloat(ban));
+
+//
+
+var f = parseInt("40 years") 
+console.log(f)
+
+var g = parseInt("He was 40") 
+console.log(g);                              //?
+
+var h = parseInt("10", 20)
+console.log(h)
+
+var i = parseInt(010)             //8
+console.log(i)
+
+
+var k = parseInt("0x10")
+console.log(k)               //16
+
