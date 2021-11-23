8fa5db54695b40698e05aa371c862407422d37ed
krishna teja <92019396+GV-KrishnaTeja@users.noreply.github.com>
2021-11-23 23:37:28 +0530

    Add files via upload
    login and logout using jwt and cookies (main one )

--- authorize.js
+++ authorize.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 var router = express.Router();
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
@@ -17,13 +18,36 @@ connection.connect(function(error){
 
 
 router.post('/authorize', function (req, res, next) {
+        const secret = 'QWER@#$^'
         const username = req.body.uname
         const pass = req.body.psw
          console.log(pass);
          console.log(username);
          connection.query("select * from loginuser where user_name = ? and user_pass=?",[username,pass],function(error,result,fields){
              if(result){
-                 res.render('anu',{ title: 'HOMEPAGE' ,result})
+                jwt.sign({ user: 'IoneTech' },secret, function(err, token) { // step -1 creating a token
+                    if (err) {
+                        console.log(err);
+                        res.status(500).send(err);
+                        return;
+                    } else {
+                        console.log('Inside');
+                        tokenGen = token;
+                        console.log(token);
+                        const tokenObj = {
+                            code : token
+                        }
+                        res.cookie('jwtToken',tokenObj,{maxAge : 300000}); // step -2 & 3 , creating a cookie an dloading token in cookie
+                        res.render('anu',{ title: 'HOMEPAGE' ,result})
+    
+                    }
+                })   
+    
+                //res.send('<h1>Check username and password!</h1>')
+                // res.render('kta', {title: 'Express'})
+            
+    
+                
              }else{
                  console.log('error')
              }
